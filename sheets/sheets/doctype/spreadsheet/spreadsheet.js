374702e446911584dfa99064a9b4f93bd3ba3f69
sokumon <sohamkulkarns9@gmail.com>
2024-11-20 22:08:24 +0530

    :feat sheet doctype created

---
+++ sheets/sheets/doctype/spreadsheet/spreadsheet.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Soham Kulkarni and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Spreadsheet", {
+// 	refresh(frm) {
+
+// 	},
+// });
